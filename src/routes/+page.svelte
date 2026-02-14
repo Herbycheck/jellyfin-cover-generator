30857e5630173779c45495a1311e316f9b7425c0
Herbycheck <eule2004.th@gmail.com>
2026-02-14 16:28:35 +0100

    Add a reference to github repo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,16 +23,18 @@
 </script>
 
 <form on:submit|preventDefault={setSecrets} class="card">
-	<h2>Please enter the url and api key for your Jellyfin instance.</h2>
-	<p>The url and api key stay in your browser and are never sent anywhere except to your server</p>
-
+	<h2 class="text-center">Please enter the url and api key for your Jellyfin instance.</h2>
 	<label for="url">Instance URL</label>
 	<input type="url" name="url" id="url" bind:value={baseUrl} />
 
 	<label for="apiKey">Api Key</label>
 	<input type="password" name="apiKey" id="apiKey" bind:value={apiKey} />
 
-	<button type="submit">Save</button>
+	<button type="submit">Get Libraries</button>
+
+	<p class="m-0 weak text-center">
+		The url and api key stay in your browser and are never sent anywhere except to your server
+	</p>
 </form>
 
 {#if paramsSet}
@@ -40,3 +42,11 @@
 		<a class="button" href="/libraries">View Libraries</a>
 	</div>
 {/if}
+
+<div class="card">
+	<p class="m-0 text-center">
+		This is an open-source project, you can view the source code on <a
+			href="https://github.com/Herbycheck/jellyfin-cover-generator">Github</a
+		>
+	</p>
+</div>
